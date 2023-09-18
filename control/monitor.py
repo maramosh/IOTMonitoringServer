927393f712e3cc007d880e86f9773fdf9179ce9a
maramosh <ma.ramosh@uniandes.edu.co>
2023-09-18 16:43:14 -0500

    Update monitor.py

--- control/monitor.py
+++ control/monitor.py
@@ -67,7 +67,7 @@ def analyze_fire():
     print("Detectando incendios...")
 
     data = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(hours=0.1))
+        base_time__gte=datetime.now() - timedelta(hours=1))
     aggregation = data.annotate(check_value=Avg('avg_value')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
